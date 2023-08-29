198eca5a69af3f4c07917963bc91ec91cad95a8a
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-08-29 08:32:38 -0300

    Projeto criado

---
+++ src/main/java/br/com/carlosjunior/smartparking/SmartParkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carlosjunior/smartparking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.smartparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
