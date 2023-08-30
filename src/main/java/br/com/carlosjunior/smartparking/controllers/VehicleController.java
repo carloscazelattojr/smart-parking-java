f6985390366ac96b3fe9c572f9cfe6173a6f38a7
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-08-29 21:50:15 -0300

    Clean and ajusts

--- src/main/java/br/com/carlosjunior/smartparking/controllers/VehicleController.java
+++ src/main/java/br/com/carlosjunior/smartparking/controllers/VehicleController.java
@@ -2,10 +2,7 @@ package br.com.carlosjunior.smartparking.controllers;
 
 import br.com.carlosjunior.smartparking.dtos.VehicleDTO;
 import br.com.carlosjunior.smartparking.services.VehicleService;
-import br.com.carlosjunior.smartparking.utils.MessagesExceptions;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -19,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
-@Tag(name = "Vehicle Controller", description = "REST API for managing vehicle models")
+@Tag(name = "Vehicle Controller", description = "REST API for managing vehicle")
 @RestController
 @RequestMapping("/vehicles")
 @RequiredArgsConstructor
